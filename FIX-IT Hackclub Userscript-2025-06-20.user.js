06ca26029adcfd95e4355b5296cafe454d325e5e
Hannes Knauss <hc.knauss@web.de>
2025-06-20 18:43:36 +0200

    restyled the header

--- FIX-IT Hackclub Userscript-2025-06-20.user.js
+++ FIX-IT Hackclub Userscript-2025-06-20.user.js
@@ -12,6 +12,25 @@
 (function() {
     'use strict';
 
+    // Restyle the Home header
+    const HomeHeaderObserver = new MutationObserver(() => {
+        const h1Element = document.querySelector('div.col-xs-12 > h1:nth-child(1)');
+        if (h1Element) {
+            // Center
+            h1Element.style.textAlign = 'center';
+
+            // Enlarge
+            h1Element.style.fontSize = '3em';
+            h1Element.style.fontWeight = 'bold';
+
+            // Change text
+            h1Element.textContent = 'Home (Restyled)';
+
+            HomeHeaderObserver.disconnect();
+        }
+    });
+    HomeHeaderObserver.observe(document.body, { childList: true, subtree: true });
+
     // Remove the "Today's Picks" section
     const TodaySPicksObserver = new MutationObserver(() => {
         const todaysPicks = document.querySelector('div.game-sort-carousel-wrapper:nth-child(2)');
