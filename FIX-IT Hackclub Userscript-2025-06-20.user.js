ed08d90673397804c2adf3ee290c131f5d576afe
Hannes Knauss <hc.knauss@web.de>
2025-06-20 17:18:47 +0200

    initial commit

---
+++ FIX-IT Hackclub Userscript-2025-06-20.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         FIX-IT Hackclub Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-06-20
+// @description  A useful userscript
+// @author       Hannes
+// @match        http://*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
