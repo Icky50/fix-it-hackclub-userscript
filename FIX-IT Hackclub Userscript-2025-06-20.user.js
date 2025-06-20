93209fbab9c0ceec10421eb4bf99052a3b8dacd2
Hannes Knauss <hc.knauss@web.de>
2025-06-20 17:57:02 +0200

    added observers that remove unwanted sections of the page

--- FIX-IT Hackclub Userscript-2025-06-20.user.js
+++ FIX-IT Hackclub Userscript-2025-06-20.user.js
@@ -2,9 +2,9 @@
 // @name         FIX-IT Hackclub Userscript
 // @namespace    http://tampermonkey.net/
 // @version      2025-06-20
-// @description  A useful userscript
+// @description  A useful userscript that declutters the Roblox homepage
 // @author       Hannes
-// @match        http://*
+// @match        https://www.roblox.com/home
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -12,5 +12,34 @@
 (function() {
     'use strict';
 
-    // Your code here...
+    // Remove the "Today's Picks" section
+    const TodaySPicksObserver = new MutationObserver(() => {
+        const todaysPicks = document.querySelector('div.game-sort-carousel-wrapper:nth-child(2)');
+        if (todaysPicks) {
+            todaysPicks.remove();
+            TodaySPicksObserver.disconnect();
+        }
+    });
+    TodaySPicksObserver.observe(document.body, { childList: true, subtree: true });
+
+    // Remove the "Sponsored" section
+    const SponsoredObserver = new MutationObserver(() => {
+        const sponsoredSection = document.querySelector('.css-koapdt-collectionCarouselContainer');
+        if (sponsoredSection) {
+            sponsoredSection.remove();
+            SponsoredObserver.disconnect();
+        }
+    });
+    SponsoredObserver.observe(document.body, { childList: true, subtree: true });
+
+    // Remove the first "Recommended for You" section
+    // You only need one "Reccommended for You" section.
+    const RecommendedObserver = new MutationObserver(() => {
+        const recommendedSection = document.querySelector('.game-home-page-container > div:nth-child(1) > div:nth-child(3)');
+        if (recommendedSection) {
+            recommendedSection.remove();
+            RecommendedObserver.disconnect();
+        }
+    });
+    RecommendedObserver.observe(document.body, { childList: true, subtree: true });
 })();
